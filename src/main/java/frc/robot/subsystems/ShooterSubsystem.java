274e5567b279528b7fca6f44a74fb3029fbadc56
sniveeey <144858102+sniveeey@users.noreply.github.com>
2026-03-03 14:44:54 -0600

    Minor shooter change

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -5,14 +5,17 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
+    private static final int SHOOTER_CAN_ID = 20;
     private static final int INDEXER_CAN_ID = 21;
-    private static final int SHOOTER_CAN_ID = 22;
+    private static final int SHOOTER_2_CAN_ID = 22;
 
     private static final double kP = 0.5;
     private static final double kI = 0.0;
@@ -24,10 +27,12 @@ public class ShooterSubsystem extends SubsystemBase {
 
     private final TalonFX indexerMotor;
     private final TalonFX shooterMotor;
+    private final TalonFX shooter2Motor;
 
     public ShooterSubsystem() {
         indexerMotor = new TalonFX(INDEXER_CAN_ID);
         shooterMotor = new TalonFX(SHOOTER_CAN_ID);
+        shooter2Motor = new TalonFX(SHOOTER_2_CAN_ID);
 
         TalonFXConfiguration config = new TalonFXConfiguration();
 
@@ -46,10 +51,11 @@ public class ShooterSubsystem extends SubsystemBase {
 
         indexerMotor.getConfigurator().apply(config);
         shooterMotor.getConfigurator().apply(config);
+        shooter2Motor.setControl(new Follower(SHOOTER_CAN_ID, MotorAlignmentValue.Opposed));
     }
 
-    public void spinIndexer() {
-        indexerMotor.set(0.4);
+    public void spinIndexer(double value) {
+        indexerMotor.set(value);
     }
 
     public void stopIndexer() {
@@ -66,7 +72,14 @@ public class ShooterSubsystem extends SubsystemBase {
 
     public Command shooterCommand() {
         return run(() -> {
-            spinIndexer();
+            spinIndexer(0.4);
+            spinShooter();
+        });
+    }
+
+    public Command intakeCommand() {
+        return run(() -> {
+            spinIndexer(-0.4);
             spinShooter();
         });
     }
